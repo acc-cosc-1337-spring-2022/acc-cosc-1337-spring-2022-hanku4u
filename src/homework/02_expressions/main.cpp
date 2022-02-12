e1c12bff5f20117503158f9fcf0bd4048ec2f274
hanku4u <98120512+hanku4u@users.noreply.github.com>
2022-02-11 20:46:43 -0600

    HW-2 complete with test cases.

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,36 @@
 ﻿//write include statements
-
+#include "expressions.h"
+#include <iostream>
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+    double meal_amount;
+    cout<<"Enter the price of the meal: \n";
+    cin>>meal_amount; //capture a meal_amount from keyboard
 
-	return 0;
+    double tip_rate;
+    cout<<"Enter percentage you would like to tip with (i.e. 10, 15, 20): \n";
+    cin>>tip_rate; //capture a tip_rate from keyboard
+
+    tip_rate = tip_rate / 100;
+
+    double tax_amount = get_sales_tax_amount(meal_amount);
+
+    double tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+    double total = meal_amount + tax_amount + tip_amount;
+
+    cout<<"Meal amount: \t"<<meal_amount<<"\n";
+    cout<<"Sales Tax: \t"<<tax_amount<<"\n";
+    cout<<"Tip: \t"<<tip_amount<<"\n";
+    cout<<"Grand Total: \t"<<total<<"\n";
+
+    return 0;
 }
--- test/homework_test/02_expressions_test/02_expressions_tests.cpp
+++ test/homework_test/02_expressions_test/02_expressions_tests.cpp
@@ -1,14 +1,26 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "variables.h"
+#include "../src/homework/02_expressions/expressions.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
-}
+//TEST_CASE("Verify Test Configuration", "verification") {
+//	REQUIRE(true == true);
+//}
+//
+//TEST_CASE("Verify sum_numbers function")
+//{
+//	REQUIRE(add_numbers(5, 5) == 10);
+//	REQUIRE(add_numbers(10, 10) == 20);
+//}
 
-TEST_CASE("Verify sum_numbers function") 
+TEST_CASE("Test get_sales_tax_amount")
 {
-	REQUIRE(add_numbers(5, 5) == 10);
-	REQUIRE(add_numbers(10, 10) == 20);
+    REQUIRE(0.675 == get_sales_tax_amount(10));
+    REQUIRE(1.35 == get_sales_tax_amount(20));
 }
 
+TEST_CASE("Test get_tip_amount")
+{
+    REQUIRE(3 == get_tip_amount(20, 0.15));
+    REQUIRE(4 == get_tip_amount(20, .2));
+}

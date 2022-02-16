3322f0b87908884b6ccc44558711ea307c613ce6
hanku4u <98120512+hanku4u@users.noreply.github.com>
2022-02-16 09:31:00 -0600

    module 03 examples

--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -1,7 +1,13 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test get option switch")
+{
+    REQUIRE("Option 1" == get_option(1));
+    REQUIRE("Option 2" == get_option(2));
+}
